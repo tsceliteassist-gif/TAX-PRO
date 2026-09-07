31deb9c1c925ef55822c42f2f195a78bd678ac98
tsceliteassist-gif <tsc.elite.assist@gmail.com>
2026-09-07 19:37:45 -0400

    Trigger Lovable sync for black red and white theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './overrides.css';
+
+// TSC Elite Tax Pro theme: black, red, and white.
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>);
